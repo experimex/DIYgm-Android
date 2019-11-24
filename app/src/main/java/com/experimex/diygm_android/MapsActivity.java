c0ff24bf94e62520e4512f6c7204765a890b8b9c
experimex <limax2012@gmail.com>
2019-11-23 22:47:23 -0500

    Add undo button and confirm dialog

--- app/src/main/java/com/experimex/diygm_android/MapsActivity.java
+++ app/src/main/java/com/experimex/diygm_android/MapsActivity.java
@@ -1,8 +1,13 @@
 package com.experimex.diygm_android;
 
 import androidx.fragment.app.FragmentActivity;
+import io.reactivex.disposables.Disposable;
+
 import com.google.android.gms.location.FusedLocationProviderClient;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.location.Location;
 import android.os.Bundle;
 import android.Manifest;
@@ -16,27 +21,37 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.polidea.rxandroidble2.RxBleClient;
+import com.polidea.rxandroidble2.scan.ScanFilter;
+import com.polidea.rxandroidble2.scan.ScanSettings;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
+import android.os.ParcelUuid;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import com.experimex.diygm_android.R;
 
 import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    RxBleClient rxBleClient;
     private GoogleMap mMap;
     TextView t;
     private FusedLocationProviderClient fusedLocationClient;
     int currentCount = 0;
     ArrayList<Marker> markers = new ArrayList<Marker>(0);
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d("STARTUP", "YES");
 
         RxPermissions rxPermissions = new RxPermissions(this);
         rxPermissions
@@ -49,6 +64,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         // At least one permission is denied
                     }
                 });
+
+        //Context context = this;
+        //rxBleClient = RxBleClient.create(context);
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
@@ -68,13 +86,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+        final Button undoButton = findViewById(R.id.undo_button);
+        undoButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                undoMarker();
+            }
+        });
+
         final Button removeAllButton = findViewById(R.id.removeall_button);
         removeAllButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 removeAllMarkers();
             }
         });
-
 
         final Button hideButton = findViewById(R.id.hide_button);
         hideButton.setOnClickListener(new View.OnClickListener() {
@@ -83,6 +107,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 layout.setVisibility(LinearLayout.GONE);
             }
         });
+
+        builder = new AlertDialog.Builder(this);
+
+        //scan();
     }
 
     @Override
@@ -90,7 +118,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         mMap.setMyLocationEnabled(true);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(40.7, -74)));
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16.0f));;
+                        }
+                    }
+                });
     }
 
     void addMarker() {
@@ -101,16 +138,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         if (location != null) {
                             LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
                             markers.add(mMap.addMarker(new MarkerOptions().position(currentLocation).title(String.valueOf(currentCount))));
-
+                            Log.d("MARKERS ADDED", Integer.toString(markers.size()));
                         }
                     }
                 });
 
     }
 
-    void removeAllMarkers() {
-        for (int i = 0; i < markers.size(); i++) {
-            markers.get(i).remove();
+    void undoMarker() {
+        if (markers.size() > 0) {
+            markers.get(markers.size()-1).remove();
+            markers.remove(markers.size()-1);
         }
+    }
+
+    void removeAllMarkers() {
+        builder.setMessage("Are you sure you want to remove all markers?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                for (int i = 0; i < markers.size(); i++) {
+                    mMap.clear();
+                    markers.clear();
+                }
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+    void scan() {
+        Disposable scanSubscription = rxBleClient.scanBleDevices(
+                new ScanSettings.Builder()
+                        .setScanMode(ScanSettings.SCAN_MODE_BALANCED)
+                        .build()/*,
+
+                new ScanFilter.Builder()
+                        .setServiceUuid(ParcelUuid.fromString("e3754285-8072-458b-a45b-94a0dab368ef"))
+                        .build() // Optional, more than one ScanFilter may be passed as varargs.
+
+                 */
+        )
+                .subscribe(
+                        scanResult -> {
+                            Log.d("SCAN_SUCCESS", "YES");
+                            Log.d("SCAN_RESULT", scanResult.toString());
+                        },
+                        throwable -> {
+                            Log.d("SCAN_SUCCESS", throwable.toString());
+                        }
+                );
+
+        // When done, just unsubscribe.
+        scanSubscription.dispose();
     }
 }
